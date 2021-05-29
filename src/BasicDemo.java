aba0dc3757abc337220d6361b54f6cdfe6e246a0
abhishekjaiswal0217 <abhishek.jaiswal_cs18@gla.ac.in>
2021-05-29 22:53:25 +0530

    Update BasicDemo.java

--- src/BasicDemo.java
+++ src/BasicDemo.java
@@ -153,6 +153,21 @@ class BasicDemo {
 		    default: System.out.println("April");
 	    }
     }
+	  static void labeledBreak() {
+	    System.out.println("\nInside labeledBreak ...");
+	    int num = 0;
+		
+	    outermost: for (int i = 0; i < 10; i++) {
+	        for (int j = 0; j < 10; j++) {    
+	            if (i == 5 && j == 5) {
+	                break outermost;
+	            }
+	            num++;
+	        }
+	    }
+		
+	    System.out.println("num: " + num); // prints 55		
+    }
 	
 
 
