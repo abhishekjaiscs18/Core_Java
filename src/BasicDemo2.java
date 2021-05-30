e81b61b8b791dab1a7f3dc16e8031ecaef2866f0
abhishekjaiswal0217 <abhishek.jaiswal_cs18@gla.ac.in>
2021-05-31 01:44:59 +0530

    created BasicDemo2.java

---
+++ src/BasicDemo2.java
@@ -0,0 +1,12 @@
+package com.abhishekJaisCs18.basics; 
+import java.util.*;
+
+class BasicDemo2 {
+	public static void main(String[] args) {
+		System.out.println("\n Inside BasicDemo2... ");
+		Date date;
+		ArrayList List;
+		java.sql.Date date1;
+
+	}
+}
