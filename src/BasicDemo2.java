fc7fb8c1368755a8492833ff47a44dfc446d4be3
abhishekjaiswal0217 <abhishek.jaiswal_cs18@gla.ac.in>
2021-05-31 02:14:45 +0530

    Update BasicDemo2.java

--- src/BasicDemo2.java
+++ src/BasicDemo2.java
@@ -52,6 +52,21 @@ class BasicDemo2 {
 		// Static method (includes overloaded methods)
 		System.out.println("\nString.valueOf(1.3): " + String.valueOf(1.3));
 	}
+	static void stringPool() {
+		  	System.out.println("\nInside stringPool ...");
+		  String s1 = "hello!";
+		  String s2 = "hello!";
+		  String s3 = "hello!".intern();
+		  String s4 = new String("hello!");	 
+		  String s5 = "lo!";
+
+    		  System.out.println("s1 == s2: " + (s1 == s2));
+  		  System.out.println("s1 == s3: " + (s1 == s3));
+   		  System.out.println("s1 == s4: " + (s1 == s4));
+  		  System.out.println("s1 == s4.intern(): " + (s1 == s4.intern()));
+  		  System.out.println("s1 == \"hel\" + \"lo!\": " + (s1 == "hel" + "lo!"));
+   		  System.out.println("s1 == \"hel\" + s5: " + (s1 == "hel" + s5));
+    }
 	
 	public static void main(String[] args) {
 		System.out.println("\n Inside BasicDemo2... ");
